20c2c9daf5137bf9c1b18005c92c64a5cf5dbb54
titus-79 <58149956+titus-79@users.noreply.github.com>
2026-03-11 13:41:14 +0100

    feat: add like class (#7)

---
+++ app/src/main/java/fr/simplon/models/Like.java
@@ -0,0 +1,31 @@
+package fr.simplon.models;
+
+import java.time.LocalDateTime;
+
+public class Like {
+    
+    private Long id;
+    private LocalDateTime createdAt;
+    
+    public Like(Long id, LocalDateTime createdAt) {
+        this.id = id;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+}
